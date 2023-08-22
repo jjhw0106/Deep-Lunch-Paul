f1329b0987bd2075c5ff3d8b435f418d575bab22
jjhw0106 <jjhw0106@jinhak.com>
2023-08-22 18:08:03 +0900

    알고리즘 실패율

--- algorithm/프로그래머스/숫자짝궁.py
+++ algorithm/프로그래머스/숫자짝궁.py
@@ -1,20 +1,25 @@
+
 def solution(x, y):
     answer = ''
 
     mapX = {str(e):0 for e in range(10)}
     mapY = {str(e):0 for e in range(10)}
-
-    k = '3'
-    print(min(mapX[k], mapY[k]))
-
-    print(mapX)    
-
-
-    # if len(mapX.keys())== 1 and list(mapX.keys())[0] == 0:
-    #     answer = "0"
-
-    # print(mapX)
+    
+    for c in x:
+        mapX[c] += 1
+    for c in y:
+        mapY[c] += 1
+    duplicate = 0
+    for i in reversed(range(10)):
+        duplicate = min(mapX[str(i)], mapY[str(i)])
+        if duplicate != 0:
+            answer += str(i) * duplicate
+    if len(set(answer)) == 1 and list(set(answer))[0] == '0':
+        answer = '0'
+        
+    if len(set(answer)) == 0:
+        answer = '-1'
 
     return answer
 
-solution("100","2345")
+print(solution("3","3"))
